b64b93246015b6c2b54dc3595ee4187c7645bb56
Jaime Marston <jaimemarston@gmail.com>
2025-03-05 15:34:42 -0500

    Guardando cambios antes de cambiar a develop

---
+++ addons/faith_and_joy_requests/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": """faith and joy requests""",
+    "version": "1.0",
+    'category': '',
+    "depends": ['base'],
+    'data': [
+        "security/ir.model.access.csv",
+        'views/requests_management_views.xml',
+        'views/menus_views.xml',
+    ],
+    'license': 'OPL-1',
+}
